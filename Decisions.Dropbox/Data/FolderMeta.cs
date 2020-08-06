182fad0a43cd1d6a86a79c3bd8e7054699641a63
ivan <kovalchuk_i_v@mail.ru>
2020-08-06 16:17:47 +0300

    share/unshare folder: waiting for the result was added

---
+++ Decisions.Dropbox/Data/FolderMeta.cs
@@ -0,0 +1,65 @@
+﻿using Dropbox.Api.Sharing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decisions.DropboxApi.Data
+{
+    [DataContract]
+    public class FolderMeta
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public FolderPolicy Policy { get;  set; }
+
+        [DataMember]
+        public string PreviewUrl { get; set; }
+
+        [DataMember]
+        public string SharedFolderId { get; set; }
+
+        [DataMember]
+        public DateTime TimeInvited { get; set; }
+
+        [DataMember]
+        public SharedContentLinkMetadata LinkMetadata { get; set; }
+
+        [DataMember]
+        public FolderPermission[] Permissions { get; set; }
+
+        [DataMember]
+        public AccessInheritance AccessInheritance { get; set; }
+
+        public AccessLevel AccessType { get; set; }
+        
+        [DataMember]
+        public bool IsInsideTeamFolder { get; set; }
+        
+        [DataMember]
+        public bool IsTeamFolder { get; set; }
+
+
+        [DataMember]
+        public IList<string> OwnerDisplayNames { get; set; }
+
+        [DataMember]
+        public string OwnerTeamId { get; set; }
+
+        [DataMember]
+        public string OwnerTeamName { get; set; }
+
+        [DataMember]
+        public string ParentSharedFolderId { get; set; }
+
+        [DataMember]
+        public string PathLower { get; set; }
+
+        [DataMember]
+        public string ParentFolderName { get; set; }
+    }
+}
---
+++ Decisions.Dropbox/Data/Sharing.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decisions.DropboxApi.Data
+{
+    [DataContract]
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum DropBoxAccessLevel { owner, editor, viewer, viewer_no_comment }
+
+    
+    class Sharing
+    {
+    }
+}
